8c03fd37e5acca15fb4b60d55061c87081fe27ee
noman081 <abdullahalnoman642@gmail.com>
2022-05-25 19:56:05 +0600

    After adding single part api

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tsxja.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const partCollection = client.db('Autoparts').collection('parts');
+
+
+        //parts api
+        app.get('/parts', async (req, res) => {
+            const result = await partCollection.find().toArray();
+            res.send(result);
+        });
+        app.get('/part/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await partCollection.findOne(query);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Manufacturer company server is running....................');
